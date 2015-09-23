75d8f5d66048fe200e2c2a11249cfde6a7237dab
Aurelio De Rosa <aurelioderosa@gmail.com>
2015-09-23 02:26:36 +0100

    Explicitly provide dependencies for the app controller

--- public/inc/js/app.js
+++ public/inc/js/app.js
@@ -2,13 +2,13 @@
  * Created by heiglandreas on 05.06.15.
  */
 angular.module('callingallpapers', ['720kb.tooltips'])
-    .controller('EventsCtrl', function($scope, $http){
+    .controller('EventsCtrl', ['$scope', '$http', function($scope, $http) {
         //$http.get('events.json').then(function(eventsResponse) {
         $http.jsonp('http://api.joind.in/v2.1/events?filter=cfp&verbose=yes&callback=JSON_CALLBACK')
             .success(function(eventsResponse) {
                 $scope.events = eventsResponse.events;
             });
-    })
+    }])
 
     .filter('dateDiff', function () {
         var magicNumber = (1000 * 60 * 60 * 24);
